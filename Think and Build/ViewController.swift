80cedf941a24f015d1dcb0fef4c3ab4a2c98977e
Mu qi <muqizhang640@gmail.com>
2020-06-09 21:43:11 +0200

    Panel para detectar toques encima de los cubos añadido

--- Think and Build/ViewController.swift
+++ Think and Build/ViewController.swift
@@ -24,13 +24,18 @@ class ViewController: UIViewController {
         return cubo
     }
     
-    func handleTapOnEntity(_ entity: Entity?) {
-        guard entity != nil else { return }
-           print("HEYYYY")
-        let cuboNuevo = crearCubo()
-        cuboNuevo.position = SIMD3(x: (entity?.position.x)!, y: (entity?.position.y)! + 0.035, z: (entity?.position.z)!)
-        tableroJuego.addChild(cuboNuevo)
-       }
+    func crearPanelArribaDelCubo(cubo: ModelEntity) {
+        let panelArribaDelCubo = ModelEntity.init(mesh: .generatePlane(width: 0.07, height: 0.07), materials: [Material].init(arrayLiteral: SimpleMaterial.init(color: SimpleMaterial.Color.white, isMetallic: false)))
+        
+        
+        panelArribaDelCubo.orientation = simd_quatf(angle: -(Float.pi/2),
+        axis: [1, 0, 0])
+        
+        
+        panelArribaDelCubo.position = SIMD3(x: (cubo.position.x), y: (cubo.position.y) + 0.038, z: (cubo.position.z))
+        
+        tableroJuego.addChild(panelArribaDelCubo)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,9 +48,21 @@ class ViewController: UIViewController {
         self.arView.scene.anchors.append(tableroJuego)
         
         
+        
         for i in 1...49 {
             tableroJuego.actions.allActions[i - 1].onAction = handleTapOnEntity(_:)
         }
-        
+        print(tableroJuego.actions.allActions.count)
     }
+    
+    func handleTapOnEntity(_ entity: Entity?) {
+           guard entity != nil else { return }
+           
+        let cuboNuevo = crearCubo()
+        cuboNuevo.position = SIMD3(x: (entity?.position.x)!, y: (entity?.position.y)! + 0.035, z: (entity?.position.z)!)
+        tableroJuego.addChild(cuboNuevo)
+        
+        crearPanelArribaDelCubo(cubo: cuboNuevo)
+        
+          }
 }
